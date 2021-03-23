2fbed1d4cb663654a12e0da9f748302e6c603140
신다정 <ssdj0607@naver.com>
2021-03-23 09:30:50 +0900

    210323

--- frontend/src/components/campsite/CampsiteListByWord.vue
+++ frontend/src/components/campsite/CampsiteListByWord.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div>
     <div
      v-for="(item, index) in tagList" :key="index">
@@ -55,36 +54,3 @@ export default {
   height: 500px;
 }
 </style>
-=======
-    <div>
-        <p>검색결과</p>
-
-        <b-form-group v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-            id="checkbox-group-1"
-            v-model="selected"
-            :options="options"
-            :aria-describedby="ariaDescribedby"
-            name="flavour-1"
-        ></b-form-checkbox-group>
-        </b-form-group>
-    </div>
-</template>
-<script> 
-export default {
-    name: 'CampsiteListByTag',
-    data() {
-      return {
-        selected: [], // Must be an array reference!
-        options: [
-          { text: '부대시설1', value: '부대시설1' },
-          { text: '부대시설2', value: '부대시설2' },
-          { text: '부대시설3', value: '부대시설3' },
-          { text: '부대시설4', value: '부대시설4' },
-          { text: '부대시설5', value: '부대시설5' },
-        ]
-      }
-    }
-}
-</script>
->>>>>>> f8e60257e4a2145bc75344de92f49ef66b90dec8
