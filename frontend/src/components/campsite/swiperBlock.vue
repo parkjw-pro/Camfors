a8774bc68f1be8491a4235e6e92c7b2a7ad37976
신다정 <ssdj0607@naver.com>
2021-04-02 11:03:31 +0900

    update

--- frontend/src/components/campsite/swiperBlock.vue
+++ frontend/src/components/campsite/swiperBlock.vue
@@ -4,12 +4,12 @@
       @click="campsiteDetail"
       v-if="item.firstImageUrlV.length > 0"
       :src="item.firstImageUrlV"
-      height="65%"
+      height="170px"
     ></b-card-img>
     <b-card-img
       v-else
       src="https://cdn.pixabay.com/photo/2019/07/25/17/09/camp-4363073_960_720.png"
-      height="65%"
+      height="170px"
     ></b-card-img>
     <span class="my-2" style="font-size:18px">{{ item.campsite_name }}</span>
     <b-card-text>{{ item.doNm }} {{ item.sigunguNm }}</b-card-text>
