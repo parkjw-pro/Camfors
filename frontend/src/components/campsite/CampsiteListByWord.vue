1e073c0b3c65443160290d4be7bad31346c3b272
Park Jongwon <whddnjs18212@naver.com>
2021-04-02 12:52:18 +0900

    mypage likeList 수정

--- frontend/src/components/campsite/CampsiteListByWord.vue
+++ frontend/src/components/campsite/CampsiteListByWord.vue
@@ -1,46 +1,60 @@
 <template>
-  <div class="row" style="margin:0 auto; width:1300px;">
-    <div v-for="(item, index) in SearchWordList" :key="index" style="margin:0 auto;">
-
-      <div class="col-md-3">
-        <swiperBlock :item = "item" style="width: 20rem; height:20rem; margin-bottom:2rem;"/>
+  <div id="box1" style="margin:0 auto;">
+    <div style="text-align: center;">
+      <br />
+      <h2
+        v-if="getSearchWordName"
+        style="text-align: left; font-family: 'Hanna', sans-serif; color: black;"
+      >
+        {{ getSearchWordName }}의 검색결과입니다.
+      </h2>
+      <br />
+      <div class="row" style="text-align: center;">
+          <div v-for="(item, index) in SearchWordList" :key="index">
+            <div class="col-md-3">
+              <swiperBlock :item="item" style="width: 20rem; height:20rem; margin-bottom:2rem;" />
+            </div>
+          </div>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script>
-import "swiper/css/swiper.css";
-import swiperBlock from "@/components/campsite/swiperBlock";
+import 'swiper/css/swiper.css';
+import swiperBlock from '@/components/campsite/swiperBlock';
+import { mapGetters } from 'vuex';
 export default {
   components: {
-    swiperBlock
+    swiperBlock,
   },
   props: {
-    SearchWordList: Array
+    SearchWordList: Array,
   },
   data: function() {
     return {
       swiperOption: {
-        direction: "vertical",
+        direction: 'vertical',
         pagination: {
-          el: ".swiper-pagination",
-          type: "bullets"
-        }
-      }
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+      },
     };
   },
   methods: {
     enlarge(event) {
-      event.currentTarget.classList.add("large");
-    }
+      event.currentTarget.classList.add('large');
+    },
   },
-  
+  computed: {
+    ...mapGetters({
+      getSearchWordName: 'campStore/getSearchWordName',
+    }),
+  },
 };
 </script>
 
-<style></style>
 <style scoped>
 .swiper-slide {
   display: flex;
@@ -49,5 +63,13 @@ export default {
 }
 .swiper-container {
   height: 500px;
+}
+#box1 {
+  display: block;
+  width: 72%;
+  position: absolute;
+  left: 14%;
+  margin-top: 0%;
+  padding-bottom: 7%;
 }
 </style>
