03014ad5a978a893173b5b0862b13c25f4da80e8
Park Jongwon <whddnjs18212@naver.com>
2021-04-05 15:26:35 +0900

    master와 merge

--- backend/camp/urls.py
+++ backend/camp/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('getwordresult/', views.campWordResult),
     path('gettagresult/', views.campTagResult),
 
-    path('gettaglist', views.gettagname),
-    path('listbyuser/<int:user_id>/', views.camplistbyuser),
+    path('gettaglist', views.gettaglist),
+    path('listbyuser/<int:user_id>/', views.listbyuser),
     path('camppoptag', views.campPopTagResult),
 
     path('addlike', views.addlike),
